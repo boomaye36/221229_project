5ad077c561b7c1e7557adbbb38b7e0201e54551f
DESKTOP-F04DRAV\김기훈 <rlgnswmfwmf@gmail.com>
2023-01-06 21:25:48 +0900

    0106

---
+++ src/main/java/com/project/config/Test.java
@@ -0,0 +1,16 @@
+package com.project.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Test {
+
+	
+	
+	@GetMapping("/test")
+	public String test() {
+		
+		return "test";
+	}
+}
